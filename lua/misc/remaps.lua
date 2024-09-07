2256df984d9d312f51cecd340869f2e017d36146
Victor <viktor.wang@gmail.com>
2024-09-07 19:18:25 -0400

    keymaps

--- lua/misc/remaps.lua
+++ lua/misc/remaps.lua
@@ -11,3 +11,8 @@ vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', 'n', 'nzz', { noremap = true, silent = true })
 vim.keymap.set('n', 'N', 'Nzz', { noremap = true, silent = true })
 
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
